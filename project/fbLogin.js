be732f8359f8f5eaf184a4a74532459ce7d815e2
Lindsay Bloom <lindsay.bloom@tufts.edu>
2017-07-16 18:28:45 -0400

    small changes

--- project/fbLogin.js
+++ project/fbLogin.js
@@ -19,10 +19,28 @@ window.fbAsyncInit = function() {
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-
-
 function checkLoginState() {
     FB.getLoginStatus(function(response) {
         statusChangeCallback(response);
+    });
+}
+
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    if (response.status === 'connected') {
+        testAPI();
+    } else {
+        document.getElementById('status').innerHTML = 'Please log ' +
+            'into this app.';
+    }
+}
+
+function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+        console.log('Successful login for: ' + response.name);
+        document.getElementById('status').innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
     });
 }
